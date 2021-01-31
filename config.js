96a260c019fffc13379bcb20dfd5ad0680386bf1
prafulpathare <patharepraful@gmail.com>
2021-01-31 12:52:52 +0530

    refresh

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 // var mongoURI = "mongodb+srv://praful:imking98@shopmartcluster.rvilr.mongodb.net/shopmart?retryWrites=true&w=majority";
 // var mongoURI = "mongodb://localhost:27017/"; // system mongo
 // var mongoURI = "mongodb://localhost:27018/"; // docker mongo_pod
